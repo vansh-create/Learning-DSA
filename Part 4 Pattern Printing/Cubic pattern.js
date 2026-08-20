9af8acf34281f1902d145a254446e455dfda0407
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-20 13:20:28 +0530

    Cubic pattern

---
+++ Part 4 Pattern Printing/Cubic pattern.js
@@ -0,0 +1,13 @@
+let prompt = require("prompt-sync")();
+
+let value = Number(prompt("Enter number of starts = "))
+
+for(let i = 1; i<=value; i++){
+
+    for(let j =1; j<=value; j++){
+        process.stdout.write("* ")
+    }
+    console.log()
+}
+
+
