41dcf88f8b71c8da5b37bf37ca8566795422a12f
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-20 14:18:47 +0530

    Mirror Right Triangle

---
+++ Part 4 Pattern Printing/Mirror Right Triangle.js
@@ -0,0 +1,17 @@
+let prompt = require("prompt-sync")();
+
+let value = Number(prompt("Enter number of starts = "))
+
+for(let i = 1; i<=value; i++){
+
+    for(let j =1; j<=value-i; j++){
+        process.stdout.write("  ")
+    }
+
+    for(let k =1; k<=i; k++){
+        process.stdout.write("* ")
+    }
+    console.log()
+}
+
+
