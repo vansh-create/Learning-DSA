6d99881aa8edef5b6beb9e5206af18de2573b4d5
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-22 00:07:02 +0530

    2nd Most Biggest Element

---
+++ Part 5 Arrays/2nd most big element.js
@@ -0,0 +1,21 @@
+let prompt = require("prompt-sync")();
+
+let arr = [10,30,40,40,40]
+
+let max = Math.max(arr[0], arr[1]);
+let max_2 = Math.min(arr[0], arr[1]);
+
+for (let i = 2; i < arr.length; i++) {
+
+   if (arr[i] > max) {
+      max_2 = max
+      max = arr[i]
+   }
+   else if (arr[i]>max_2 && max != arr[i]) {
+    max_2=arr[i]
+   }
+
+}
+
+console.log(max)
+console.log(max_2)
