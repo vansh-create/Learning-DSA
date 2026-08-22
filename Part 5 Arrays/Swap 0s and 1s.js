645988a9258b19d4ffa260e34391861eef189870
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-22 15:47:19 +0530

    sort 0s and 1s

--- Part 5 Arrays/Swap 0s and 1s.js
+++ Part 5 Arrays/Swap 0s and 1s.js
@@ -1,7 +1,15 @@
-let prompt = require("prompt-sync")
 
-let arr = [0, 1, 0, 1, 0, 1, 1, 0, 0]
+let arr = [0, 1, 0, 1, 0, 1, 1, 0, 0,0, 0, 1]
 
-for(let i=0; i<arr.length; i++){
-    
+let j = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        j++
+    }
+
 }
+
+console.log(arr)
