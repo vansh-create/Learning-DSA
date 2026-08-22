7c8d062fd35504bc32579c46852e6ead99cf58bc
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-22 15:32:30 +0530

    Reverse Array without extra space

---
+++ Part 5 Arrays/Reverse array without extra space.js
@@ -0,0 +1,19 @@
+let prompt = require("prompt-sync")();
+
+let arr = [10, 0, 20, 40, 6, 3, 3, 2, 1]
+
+let j = arr.length - 1
+
+for(let i = 0; i<=arr.length/2; i++){
+  let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    j--
+}
+
+
+  
+
+
+
+console.log(arr)
