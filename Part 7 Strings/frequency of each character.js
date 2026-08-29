ec1cfdb159e83935a459e988a982eaecfb62a6a2
Vansh Kesharwani <vanshkesharwani569@gmail.com>
2026-08-29 23:00:10 +0530

    modify the frequency of character code

--- Part 7 Strings/frequency of each character.js
+++ Part 7 Strings/frequency of each character.js
@@ -1,12 +1,27 @@
-let str = "abbccc";
+let str = "hello";
 let arr = new Array(122).fill(0)
+let visited = []
 
-for(let i=0; i<str.length; i++){
-   arr[str.charCodeAt(i)] += 1 
+for (let i = 0; i < str.length; i++) {
+    arr[str.charCodeAt(i)] += 1
 }
 
-for(let j = 1; j<arr.length; j++){
-    if(arr[j] != 0){
-        console.log(String.fromCharCode(j) + " " + arr[j])
-    }   
+// for(let j = 0; j<arr.length; j++){
+//     if(arr[j] != 0){
+//         console.log(String.fromCharCode(j) + " " + arr[j])
+//     }   
+// }
+
+// Print in orderd manner of given string : modified version
+
+for (let i = 0; i < str.length; i++) {
+
+    let code = str.charCodeAt(i)
+
+    if (visited.includes(str[i])) {
+        continue;
+    }
+
+    visited[i] = str[i]
+    console.log(String.fromCharCode(code) + " " + arr[code])
 }
